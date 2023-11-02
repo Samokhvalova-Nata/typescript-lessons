0acaad92d6859c8b5791f73a65bb20ad54d88137
Nataliia Samokhvalova <samohvalova520@gmail.com>
2023-11-02 18:36:01 +0200

    Add 1 and 3 generic task

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -2,8 +2,9 @@
   Є функція getPromise(), яка повертає проміс, що дозволяється в масив, що містить рядки та числа. 
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
+type P = string | number;
 
-function getPromise () {
+function getPromise (): Promise<P[]> {
   return new Promise((resolve) => {
     resolve(['Text', 50]);
   });
